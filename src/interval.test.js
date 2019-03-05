9e422c0870c355113480552c4d29e099058ec1d6
Antoine CHAPUSOT <antoine.chapusot@ensiie.fr>
2019-03-05 15:55:36 +0100

    overlap tests

---
+++ src/interval.test.js
@@ -0,0 +1,29 @@
+const Interval = require('./interval');
+
+describe('overlaps', function() {
+  let regular = new Interval(5, 10);
+  let ahead = new Interval(1, 3);
+  let behind = new Interval(11, 13);
+  let neigha = new Interval(4, 7);
+  let neighb = new Interval(7, 11);
+  let bigger = new Interval(3, 12);
+  let smaller = new Interval(6, 9);
+  test('Overlap NOK ahead', () => {
+    expect(regular.overlaps(ahead)).toBe(false);
+  });
+  test('Overlap NOK behind', () => {
+    expect(regular.overlaps(behind)).toBe(false);
+  });
+  test('Overlap OK ahead', () => {
+    expect(regular.overlaps(neigha)).toBe(true);
+  });
+  test('Overlap OK behind', () => {
+    expect(regular.overlaps(neighb)).toBe(true);
+  });
+  test('Overlap OK bigger', () => {
+    expect(regular.overlaps(bigger)).toBe(true);
+  });
+  test('Overlap OK smaller', () => {
+    expect(regular.overlaps(smaller)).toBe(true);
+  });
+});
